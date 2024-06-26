ceea35d86b0d2249c83601099e42219dcfcf8f13
Jaimin Pancholy <jaimin.citrusbug@gmail.com>
2024-06-26 17:41:40 +0530

    Final commit of the day

--- effects/assets/js/effects.js
+++ effects/assets/js/effects.js
@@ -1,18 +1,30 @@
-$(document).ready(function(){
-    $("#hide").click(function(){
-      $("p:first").hide();
-    });
-    $("#show").click(function(){
-      $("p:first").show();
-    });
-    $("button").click(function(){
-      $("#div1").fadeIn();
-      $("#div2").fadeIn("slow");
-      $("#div3").fadeIn(3000);
-    });
-    $("#flip").click(function(){
-      $("#panel").slideToggle("slow");
-     
-      
+$(document).ready(function () {
+  $("#hide").click(function () {
+    $("p:first").hide();
+  });
+  $("#show").click(function () {
+    $("p:first").show();
+  });
+  $("#btn1").click(function () {
+    $("#div1").fadeToggle();
+    $("#div2").fadeToggle("slow");
+    $("#div3").fadeToggle(3000);
+  });
+  $("#flip").click(function () {
+    $("#panel").slideToggle("slow");
+  });
+    $("#btn2").click(function () {
+      $("#div4").fadeTo("fast", 0.15);
+      $("#div5").fadeTo("fast", 0.4);
+      $("#div6").fadeTo("fast", 0.7);
+    
+  });
+  $("#btn3").click(function(){
+    $("#div8").animate({
+      left: '250px',
+      opacity: '0.5',
+      height: '150px',
+      width: '150px'
     });
   });
+});
