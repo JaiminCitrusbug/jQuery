47a30964edfb32934030538548055c986c750ac9
Jaimin Pancholy <jaimin.citrusbug@gmail.com>
2024-06-28 17:58:04 +0530

    Final commit of the day

--- ajax_task/ajaxtask.js
+++ ajax_task/ajaxtask.js
@@ -6,7 +6,7 @@
 //       dataType:"json",
 //       success: function (result) {
 //         $(".tbody").html(result);
-        
+
 //       },
 //     });
 //   });
@@ -23,7 +23,7 @@ $(document).ready(function () {
         //CONSTRUCTION OF ROWS HAVING
         // DATA FROM JSON OBJECT
         x += "<tr>";
-        x += "<td>" + value.userId + "</td>";
+        x += `<td> ${value.userId}  </td>`;
 
         x += "<td>" + value.id + "</td>";
 
@@ -31,12 +31,11 @@ $(document).ready(function () {
 
         x += "<td>" + value.body + "</td>";
 
-        x += "<td>" + $(`<button id="icon">Icons</button>`) + "</td>";
-
-        // x += "<td>" + $(`<button id="icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
-        //     <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
-        //     <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
-        //   </svg></button>`) + "</td>";
+       
+        x += "<td>" + `<button class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
+            <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
+          </svg></button>` + "</td>";
 
         x += "</tr>";
       });
